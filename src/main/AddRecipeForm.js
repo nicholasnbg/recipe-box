940ca8e2ef93978ea85fa7f36b775aa3387afaf3
nicholasnbg <nicholasnbg@gmail.com>
2018-03-05 22:33:02 +0900

    added validation to addRecipeForm and edited overall styling

--- src/main/AddRecipeForm.js
+++ src/main/AddRecipeForm.js
@@ -3,6 +3,44 @@ import './AddRecipeForm.css';
 
 class AddRecipeForm extends React.Component{
 
+    state= {
+        name: '',
+        ingredients: '',
+        nameValid: false,
+        ingredientsValid: false,
+        formValid: false
+    }
+
+    handleUserInput = e => {
+        const name = e.target.name
+        const value = e.target.value;
+        this.setState({[name]: value});
+        this.validateFields(name);
+    }
+
+    validateFields = name => {
+        if(this.state.name !==''){
+            this.setState({[`${name}Valid`]: true})
+        }
+        this.isFormValid();
+    }
+
+    isFormValid = () => {
+        if(this.state.nameValid && this.state.ingredientsValid){
+            this.setState({formValid: true});
+        }
+    }
+
+    resetStateValidation = () => {
+        this.setState({
+            name: '',
+            ingredients: '',
+            nameValid: false,
+            ingredientsValid: false,
+            formValid: false
+        });
+    }
+
     createRecipe(event){
         event.preventDefault();
         console.log('creating recipe');
@@ -13,9 +51,11 @@ class AddRecipeForm extends React.Component{
         }
         this.props.addRecipe(recipe);
         this.recipeForm.reset();
+        this.resetStateValidation();
     }
 
     render(){
+        let formValid = this.state.formValid;
 
         return(
             <div className="addRecipeForm">
@@ -23,10 +63,10 @@ class AddRecipeForm extends React.Component{
                 <form ref={(input) => this.recipeForm = input}
                 className="addRecipe"
                 onSubmit={(e)=> this.createRecipe(e)}>
-                    <input ref={(input)=> this.name = input} type="text" placeholder="Recipe Name"/>
-                    <textarea ref={(input)=>this.ingredients = input} type="text" placeholder="List of ingredients"></textarea>
+                    <input ref={(input)=> this.name = input} onChange={e => this.handleUserInput(e)} type="text" name="name" placeholder="Recipe Name"/>
+                    <textarea ref={(input)=>this.ingredients = input} onChange={e => this.handleUserInput(e)} type="text" name="ingredients" placeholder="List of ingredients"></textarea>
                     <span>Please seperate ingredients with a comma ","</span>
-                    <button type="submit">+ Add Recipe</button>
+                    <button type="submit" disabled={!this.state.formValid}>+ Add Recipe</button>
                 </form>
             </div>
         )
